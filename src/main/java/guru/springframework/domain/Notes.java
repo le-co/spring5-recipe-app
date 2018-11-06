d05d7b1f8ca6da0c62a3c497b81c845f8175e1a3
leco <lcosta.sp.br@gmail.com>
2018-11-06 20:46:01 -0200

    create notes and recipe entities

---
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -0,0 +1,41 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipNotes;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipNotes() {
+        return recipNotes;
+    }
+
+    public void setRecipNotes(String recipNotes) {
+        this.recipNotes = recipNotes;
+    }
+}
