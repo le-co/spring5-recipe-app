5b925625ab002b78507453c8fa3b34bc4b98697d
leco <lcosta.sp.br@gmail.com>
2018-11-11 15:14:20 -0200

    create recipe page, and load guaca mole recipe

--- src/main/java/guru/springframework/bootstrap/LoadRecipes.java
+++ src/main/java/guru/springframework/bootstrap/LoadRecipes.java
@@ -50,13 +50,36 @@ public class LoadRecipes implements CommandLineRunner {
         recipe.setPrepTime(10);
         recipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
 
-//        File file = new File("../../resource/guaca-mole.jpg");
-//        byte[] images = Files.readAllBytes(file.toPath());
-//        Byte[] bytes = new Byte[images.length];
-//        for (int i = 0; i < images.length; i++) {
-//            bytes[i] = Byte.valueOf(images[i]);
-//        }
-//        recipe.setImage(bytes);
+        recipe.setDirections("1 Cut avocado, remove flesh: Cut the avocados in half. Remove seed. Score the inside of " +
+                "the avocado with a blunt knife and scoop out the flesh with a spoon. (See How to Cut and Peel an Avocado.)" +
+                " Place in a bowl." +
+                "2 Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.)" +
+                "3 Add salt, lime juice, and the rest: Sprinkle with salt and lime (or lemon) juice. The acid in the lime juice will provide" +
+                " some balance to the richness of the avocado and will help delay the avocados from turning brown.\n" +
+                "\n" +
+                "Add the chopped onion, cilantro, black pepper, and chiles. Chili peppers vary individually in their hotness." +
+                " So, start with a half of one chili pepper and add to the guacamole to your desired degree of hotness.\n" +
+                "\n" +
+                "Remember that much of this is done to taste because of the variability in the fresh ingredients. Start with " +
+                "this recipe and adjust to your taste." +
+                "4 Cover with plastic and chill to store: Place plastic wrap on the surface of the guacamole cover it and to" +
+                " prevent air reaching it. (The oxygen in the air causes oxidation which will turn the guacamole brown.) " +
+                "Refrigerate until ready to serve.\n" +
+                "\n" +
+                "Chilling tomatoes hurts their flavor, so if you want to add chopped tomato to your guacamole, add it just before serving.\n" +
+                "\n" +
+                "Variations\n" +
+                "\n" +
+                "For a very quick guacamole just take a 1/4 cup of salsa and mix it in with your mashed avocados.\n" +
+                "\n" +
+                "Feel free to experiment! One classic Mexican guacamole has pomegranate seeds and chunks of peaches in it" +
+                " (a Diana Kennedy favorite). Try guacamole with added pineapple, mango, or strawberries (see our Strawberry Guacamole).\n" +
+                "\n" +
+                "The simplest version of guacamole is just mashed avocados with salt. Don't let the lack of availability of other ingredients" +
+                " stop you from making guacamole.\n" +
+                "\n" +
+                "To extend a limited supply of avocados, add either sour cream or cottage cheese to your guacamole dip. Purists may be " +
+                "horrified, but so what? It tastes great.");
 
         Set<Ingredient> ingredients = new HashSet<>();
 
