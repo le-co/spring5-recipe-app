10e04eca7f43f0a0ce2c55852d96bbdaaf432e7c
leco <lcosta.sp.br@gmail.com>
2018-11-25 13:06:34 -0200

    configure tomcat application

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -2,11 +2,12 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Spring5RecipeAppApplication {
+public class Spring5RecipeAppApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5RecipeAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeAppApplication.class, args);
+    }
 }
