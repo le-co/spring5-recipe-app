f1aa0083c33f3603ba8d8ac87e1f68b1cb26cf4d
leco <lcosta.sp.br@gmail.com>
2018-12-18 19:36:20 -0200

    configure constraints validation

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -4,7 +4,10 @@ import guru.springframework.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,14 +18,27 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 public class RecipeCommand {
+
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
     private Integer prepTime;
+
     private Integer cookTime;
+
     private Integer servings;
+
     private String source;
+
+    @URL
     private String url;
+
+    @Size(min = 3, max = 255)
     private String directions;
+
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Byte[] image;
     private Difficulty difficulty;
--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,14 +1,17 @@
 package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
-import guru.springframework.exceptions.NotFoundResultException;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 /**
  * Created by jt on 6/19/17.
@@ -16,6 +19,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Slf4j
 @Controller
 public class RecipeController {
+
+    static final String RECIPE_RECIPEFORM = "recipe/recipeform";
 
     private final RecipeService recipeService;
 
@@ -35,17 +40,24 @@ public class RecipeController {
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
 
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM;
     }
 
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+    public String saveOrUpdate(@Valid @ModelAttribute RecipeCommand command, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(error -> {
+                log.debug("Error : {}", error);
+            });
+            return RECIPE_RECIPEFORM;
+        }
+
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
         return "redirect:/recipe/" + savedCommand.getId() + "/show";
